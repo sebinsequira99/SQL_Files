38b8675f74352a532fe0e867914ef5efb392a556
Sebin Sequira <sebinsequira99@gmail.com>
2016-10-28 19:38:03 +0530

    changes

--- new/aaimerge task status.sql
+++ new/aaimerge task status.sql
@@ -1,29 +1,15 @@
 
 
-aaimerge
+mapcssmt
 
-user_groups where group_name like '%international%application%'
-user_groups where group_name like '%domestic%application%'
-
-formmaker_usergroups where pg_group_id in(
-944
-,945
-)
-
-formmaker_submissions
-
-formmaker_taskstatus where groupid like '%944%' or groupid like '%945%'
-formmaker_taskstatus where phase_id in(2059
-,2060
-,2058
-,2062
-,2061)
+usp_RptTaskStatus
+/MAPSSRS/Rpt
 
 drop table #result
 
 select p.phase,ft.phase_id,ft.stage_id,ft.task_id,ft.task_status
 ,r.connectionid,r.contactid,ft.user_id,replace(r.displayname,'amp;','') as clientname
-,r.rellabel,rt.relationshiptypetext,ft.startdate
+,r.rellabel,rt.relationshiptypetext,r.connectionstatusid,lc.connectionstatustext,ft.startdate
 into #result
 from formmaker_taskstatus ft
 inner join formmaker_phases p
@@ -32,11 +18,8 @@ inner join rel_relationship r
 on ft.connectionid=r.connectionid and abs(ft.connid)=r.contactid
 inner join rel_lkp_relationshiptype rt
 on r.relationshiptypeid=rt.relationshiptypeid
-where ft.phase_id in(2059
-,2060
-,2058
-,2062
-,2061)
+inner join lkpconnectionstatus lc
+on r.connectionstatusid=lc.connectionstatusid
 
 alter table #result
 add taskname varchar(max) 
@@ -45,7 +28,13 @@ alter table #result
 add GroupName varchar(max) 
 
 alter table #result
-add effectivedate datetime
+add casestatus_startdate datetime
+
+alter table #result
+add email varchar(500)
+
+alter table #result
+add phone varchar(100)
 
 update r
 set r.taskname=t.task
@@ -66,14 +55,25 @@ SET a.GroupName=STUFF((SELECT distinct ',' + g.group_name
 							  ,1,1,'')
 FROM #result a
 
-Update #result 
-Set taskname=REPLACE(REPLACE(taskname,CHAR(13), ' '), CHAR(10), ' ')
+update r
+set r.casestatus_startdate=c.ConnectionStatusStartDate
+from #result r
+inner join connectionstatuslog c
+on r.connectionid=c.connectionid
+and r.connectionstatusid=c.connectionstatusid
 
 update r
-set r.effectivedate=e.effectivedate
+set r.email=ce.ContactEmail
 from #result r
-inner join user_effectivedate e
-on r.user_id=e.user_id
-and (e.group_id=944 or e.group_id=945)
+inner join contactemail ce
+on r.contactid=ce.contactid
+and ce.PrimaryEmail=1
 
+update r
+set r.phone=cp.PhoneNumber
+from #result r
+inner join contactphone cp
+on r.contactid=cp.contactid
+and cp.PrimaryPhone=1
 
+select * into rpttaskstatus from #result
